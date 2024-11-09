124065a5477b14499005bb6f7792618e5a668865
Bethwel Kiplagat <sjemnyango9@gmail.com>
2024-11-09 13:54:48 +0300

    backend logic done

--- database/database.py
+++ database/database.py
@@ -5,7 +5,7 @@ def connect():
     return psycopg2.connect(
         dbname='postgres',
         user='postgres',
-        password='12345',
+        password='bethwel3001',
         host='localhost',
         port='5432'
     )
